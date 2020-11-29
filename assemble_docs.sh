57b1e590fddd082fc07794d9fa7b83599b10b1d7
Volodymyr Buberenko <v.buberenko@gmail.com>
2020-11-29 19:59:27 +0200

    Add workflow to update MkDocs file

--- assemble_docs.sh
+++ assemble_docs.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-# Copy outside files into the docs folder.
-sed -e '/full documentation here/ { N; d; }' < README.md > docs/index.md
+# Copy changelog from root to make it available for Mkdocs
 cp CHANGELOG.md docs/changelog.md
-cp coil-gif/README.md docs/gifs.md
-cp coil-svg/README.md docs/svgs.md
-cp coil-video/README.md docs/videos.md
-cp logo.svg docs/logo.svg
-cp README-ko.md docs/README-ko.md
-cp README-zh.md docs/README-zh.md
 
-# Clean up.
-rm package-list-coil-base package-list-okio
+# Deploy to Github pages.
+mkdocs gh-deploy
